46fcab828ac0957898c7e42a3cc17244eac8c7a6
Mark Joseph Sinnaco <markjosephsinnaco@gmail.com>
2019-03-12 18:23:24 +0800

    Edit Update and Delete

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -70,7 +70,9 @@ class ShareController extends Controller
      */
     public function edit($id)
     {
-        //
+        $share = Share::find($id);
+
+        return view('shares.edit', compact('share'));
     }
 
     /**
@@ -82,7 +84,19 @@ class ShareController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'share_name'=>'required',
+            'share_price'=> 'required|integer',
+            'share_qty' => 'required|integer'
+          ]);
+    
+          $share = Share::find($id);
+          $share->share_name = $request->get('share_name');
+          $share->share_price = $request->get('share_price');
+          $share->share_qty = $request->get('share_qty');
+          $share->save();
+    
+          return redirect('/shares')->with('success', 'Stock has been updated');
     }
 
     /**
@@ -93,6 +107,9 @@ class ShareController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $share = Share::find($id);
+        $share->delete();
+
+        return redirect('/shares')->with('success', 'Stock has been deleted Successfully');
     }
 }
